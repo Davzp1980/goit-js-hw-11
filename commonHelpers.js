1b9e3d8784a49e89b5ea2eaa6e1daa07d4053c0d
Davzp1980 <davzp1980@gmail.com>
2024-06-11 18:31:45 +0000

    Deploying to gh-pages from @ Davzp1980/goit-js-hw-11@404d1148d29b23a2ee3b760679480c071308035e 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,34 +1,34 @@
-import{s as d,i as u}from"./assets/vendor-5c957d73.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(e){if(e.ep)return;e.ep=!0;const i=r(e);fetch(e.href,i)}})();const m="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAEaSURBVHgBrZZhDoIwDIUL0fjXo3gUb7R5Er0BHsXD7A+bASlhYeC6V4WXGCHr3reNtkAkqG2D9T70mh/HSj4HybyqyPQ9PeuaXlRQ19GFY4c5dDodLSHNK3/fSam29Q+0k7/N1ZAt5hCyh7kIKZl77y/IMBezgEwXTW4ienBxcc65DCQ0PDYCQghGMDASJJpzTG4ue0KABEHmKeBAQEPx3LiIhmKy/M+aitDyGJoPAWsI32vNWTXpVQnXRal2kPQmO7pPx6XpPRCQmsdjSZ8JhIA0FbOllMKsRZqCQoMpXCy0GJRrFc7hVpGLSVqFWaxk52ZnVgPbIaL5HhBovgVSMv+qg7kt9JznZ+VL//pL+5hWFXei+mwRj+UDSHQ9vnHBEYAAAAAASUVORK5CYII=";function p(t){return`<li class="list-item">
+import{s as u,i as p}from"./assets/vendor-5c957d73.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))l(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function s(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerPolicy&&(i.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?i.credentials="include":t.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(t){if(t.ep)return;t.ep=!0;const i=s(t);fetch(t.href,i)}})();const m="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAEaSURBVHgBrZZhDoIwDIUL0fjXo3gUb7R5Er0BHsXD7A+bASlhYeC6V4WXGCHr3reNtkAkqG2D9T70mh/HSj4HybyqyPQ9PeuaXlRQ19GFY4c5dDodLSHNK3/fSam29Q+0k7/N1ZAt5hCyh7kIKZl77y/IMBezgEwXTW4ienBxcc65DCQ0PDYCQghGMDASJJpzTG4ue0KABEHmKeBAQEPx3LiIhmKy/M+aitDyGJoPAWsI32vNWTXpVQnXRal2kPQmO7pPx6XpPRCQmsdjSZ8JhIA0FbOllMKsRZqCQoMpXCy0GJRrFc7hVpGLSVqFWaxk52ZnVgPbIaL5HhBovgVSMv+qg7kt9JznZ+VL//pL+5hWFXei+mwRj+UDSHQ9vnHBEYAAAAAASUVORK5CYII=";function A(e){return`<li class="list-item">
           <div class="img-box">
             <a
               class="gallery-link"
-              href="${t.largeImageURL}"
+              href="${e.largeImageURL}"
             >
               <img
-                src="${t.previewURL}"
-                alt="${t.tags}"
-                width="360"
+                src="${e.previewURL}"
+                alt="${e.tags}"
+                title="${e.tags}"
               />
             </a>
           </div>
           <ul class="description-list">
             <li class="description-item">
               <p>Likes</p>
-              <p>${t.likes}</p>
+              <p>${e.likes}</p>
             </li>
             <li class="description-item">
               <p>Views</p>
-              <p>${t.views}</p>
+              <p>${e.views}</p>
             </li>
             <li class="description-item">
               <p>Comments</p>
-              <p>${t.comments}</p>
+              <p>${e.comments}</p>
             </li>
             <li class="description-item">
               <p>Downloads</p>
-              <p>${t.downloads}</p>
+              <p>${e.downloads}</p>
             </li>
           </ul>
-        </li>`}function A(t){return t.map(p).join("")}const h=new d(".img-list a"),f={iconUrl:m,backgroundColor:"#ef4040",position:"topRight",message:"Sorry, there are no images matching your search query. Please, try again!"},a=document.querySelector(".img-list"),c=document.querySelector(".loading");function g(t){const s=`https://pixabay.com/api/?key=44327397-ede54b0a70b202831c7c411c5&q=${t}&image_type=photo
-  &orientation=horizontal&safesearch=true`;fetch(s).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{if(r.hits.length===0){u.show(f),c.classList.add("visually-hidden");return}a.innerHTML=A(r.hits),c.classList.add("visually-hidden"),h.refresh()}).catch(r=>{throw new Error(r)})}const n=document.querySelector(".search-form");document.querySelector(".search-btn");const y=document.querySelector(".loading");n.addEventListener("submit",t=>{t.preventDefault(),a.innerHTML="",y.classList.remove("visually-hidden");const s=n.elements.searchText.value.trim();s!==""&&(g(s),n.reset())});
+        </li>`}function h(e){return e.map(A).join("")}const f=new u(".img-list a",{captionDelay:500,captionPosition:"bottom"}),g={iconUrl:m,backgroundColor:"#ef4040",position:"topRight",message:"Sorry, there are no images matching your search query. Please, try again!"},d=document.querySelector(".img-list"),c=document.querySelector(".loader");function y(e){const r=`https://pixabay.com/api/?key=44327397-ede54b0a70b202831c7c411c5&q=${e}&image_type=photo
+  &orientation=horizontal&safesearch=true`;fetch(r).then(s=>{if(!s.ok)throw new Error(s.status);return s.json()}).then(s=>{if(s.hits.length===0){p.show(g),c.classList.add("visually-hidden");return}console.log(s.hits),c.classList.add("visually-hidden"),d.innerHTML=h(s.hits),f.refresh()}).catch(s=>{throw new Error(s)})}const n=document.querySelector(".search-form");document.querySelector(".search-btn");const a=document.querySelector(".loader");n.addEventListener("submit",e=>{e.preventDefault(),d.innerHTML="",a.classList.remove("visually-hidden");const r=n.elements.searchText.value.trim();if(r===""){a.classList.add("visually-hidden");return}y(r),n.reset()});
 //# sourceMappingURL=commonHelpers.js.map
